f6f3c8c6dae209a6d45c12c09af5bf7788ebb390
Aaron Reed <aareed@gmail.com>
2016-07-10 18:28:07 -0700

    Rough draft of schema edit window

--- ensemblejs/ensembletool/schemaviewer.js
+++ ensemblejs/ensembletool/schemaviewer.js
@@ -46,15 +46,113 @@ define(["ensemble", "jquery"], function(ensemble, $){
 			if (d.actionable) {
 				catClass += " actionable";
 			}
-			exp += "<div class='" + catClass + "'>" + thisBlock + "</div>";
+			exp += "<div class='" + catClass + "' id='cat_" + categoryName + "'>" + thisBlock + "</div>";
 		}
-		exp += "<div class='schemaCategory'><div class='schemaHeader'><span class='categoryNew'>+ New</span></div><div class='editIcon'><div class='edit1'>&nbsp;</div><div class='edit2'>&nbsp;</div></div></div>";
+		// Add "New Schema Category" button
+		exp += "<div class='schemaCategory'><div class='schemaHeader'><span class='categoryNew'>+ New</span></div><div class='editIcon'><div class='edit1'>&nbsp;</div><div class='edit2'>&nbsp;</div></div></div>"; 
 		$("#infoOnSocialTypes").html(exp);
+
+		// Show/Hide edit icon on hover.
 		$(".schemaCategory").hover(function() {
 			$(this).children(".editIcon").show();
 		}, function() {
 			$(this).children(".editIcon").hide();
 		});
+
+		$(".schemaCategory").click(editSchemaCategory);
+	}
+
+	var editSchemaCategory = function() {
+		var category = $(this).attr("id").split("_")[1]; // i.e. "cat_trait"
+		var cat = socialStructure[category];
+		var catDescriptors = ensemble.getCategoryDescriptors(category);
+		var html = "<div id='schemaEditForm'>";
+		html += "<div><label for='editorCategoryName'>Category Name</label> <input id='editorCategoryName' type='text' value='" + category + "'/></div>";
+		html += "<div><label for='editorIsBoolean'>Data Type</label> <input type='radio' id='isBooleanT' name='isBoolean' value='T'>True/False <input type='radio' id='isBooleanF' name='isBoolean' value='F'>Number</div>";
+		html += "<div><label for='editorDirection'>Direction</label> <input type='radio' id='dirUn' name='directionType' value='undirected'>Undirected <input type='radio' id='dirDir' name='directionType' value='directed'>Directed <input type='radio' id='dirRecip' name='directionType' value='reciprocal'>Reciprocal</div>";
+		html += "<div><label for='editorDefaultValue'>Default</label> ";
+		if (catDescriptors.isBoolean) {
+			html += "<input type='radio' id='defaultValT' name='defaultValue' value='T'>True <input type='radio' id='defaultValF' name='defaultValue' value='F'>False";
+		} else {
+			html += "<input id='editorDefaultValue' type='text' value='" + catDescriptors.defaultVal + "'/>"
+		}
+		html += "</div>";
+
+		// min/max
+		if (!catDescriptors.isBoolean) {
+			html += "<div><label for='editorMin'>Min/Max</label> <input id='editorMin' type='text' value='" + catDescriptors.min + "'/> <input id='editorMax' type='text' value='" + catDescriptors.max + "'/></div>";
+		}
+
+		// Duration
+		html += "<div><label for='editorShowDuration'>Duration?</label> <input id='editorShowDuration' type='checkbox'/> <span id='editorDurationNumArea'><input id='editorDuration' type='text' value='" + (catDescriptors.duration || 3) + "'/></span></div>";
+
+		// actionable
+		html += "<div><label for='editorActionable'>Actionable?</label> <input id='editorActionable' type='checkbox'/></div>";
+
+		// Types
+		html += "<div><label>Types</label> ";
+		var types = Object.keys(cat);
+		types.forEach(function(type) {
+			html += "<input class='schemaEdType' id='schemaEd_" + type + "' value='" + type + "'/> ";
+		});
+		html += "<span class='schemaEdType schemaEdNewType'>(new)</span> ";
+		html += "</div>"
+
+		$("#dialogBox").html(html);
+
+		// Set proper values
+		var correctIsBooleanButton = catDescriptors.isBoolean ? "isBooleanT" : "isBooleanF";
+		$("#" + correctIsBooleanButton).prop("checked", true);
+
+		var correctDirectionButton;
+		if (catDescriptors.directionType === "directed") {
+			correctDirectionButton = "dirDir";
+		} else if (catDescriptors.directionType === "undirected") {
+			correctDirectionButton = "dirUn";
+		} else {
+			correctDirectionButton = "dirRecip";
+		}
+		$("#" + correctDirectionButton).prop("checked", true);
+
+		if (catDescriptors.isBoolean) {
+			var correctDefaultValButton = catDescriptors.defaultVal ? "defaultValT" : "defaultValF";
+		}
+		$("#" + correctDefaultValButton).prop("checked", true);
+
+		if (catDescriptors.duration === undefined) {
+			$("#editorShowDuration").prop("checked", false);
+			$("#editorDurationNumArea").hide();
+		} else {
+			$("#editorShowDuration").prop("checked", true);
+			$("#editorDurationNumArea").prop("value", catDescriptors.duration);
+		}
+		if (catDescriptors.actionable) {
+			$("#editorActionable").prop("checked", true);
+		}
+
+		$("#editorShowDuration").change(function() {
+			if (this.checked) {
+				$("#editorDurationNumArea").show();
+			} else {
+				$("#editorDurationNumArea").hide();
+				// TODO: Set model to have no duration
+			}
+		});
+
+		var editor = $("#dialogBox").dialog({
+			title: "Edit Schema Category",
+			resizable: false,
+			modal: true,
+			width: 550,
+			buttons: {
+				"Save Changes": function() {
+					alert("Not yet implemented.");
+				},
+				Cancel: function() {
+					$(this).dialog("destroy");
+				}
+			}
+		})
 	}
 
 	return {
